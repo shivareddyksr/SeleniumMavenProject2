4c1763ccc693743d6b1167b4083eff45c16c8c58
Shiva Reddy <shivareddyksr@gmail.com>
2023-04-26 23:07:30 +0530

    firstcommit

---
+++ src/main/java/SeleCucumberProject4/SeleCucumberProject4/App.java
@@ -0,0 +1,13 @@
+package SeleCucumberProject4.SeleCucumberProject4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/TestPackage1/Test1.java
@@ -0,0 +1,25 @@
+package TestPackage1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+
+	@Test
+	public void tc1() throws InterruptedException
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.amazon.in/");
+		Thread.sleep(3000);
+		driver.close();
+		
+	}
+}
