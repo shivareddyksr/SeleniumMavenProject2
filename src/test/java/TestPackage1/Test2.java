988a92ed20092bab8b99c18f80ea5ffd0c3d4dbe
Shiva Reddy <shivareddyksr@gmail.com>
2023-04-27 17:56:00 +0530

    fourth

--- src/test/java/TestPackage1/Test2.java
+++ src/test/java/TestPackage1/Test2.java
@@ -18,7 +18,7 @@ public class Test2 {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://www.facebook.com/");
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		driver.close();
 		
 	}
